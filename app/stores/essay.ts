6a6e29aff173262cb092d82e3e31e43b969163d4
Riva Almero <riva@dev.netkromsolution.com>
2025-02-18 12:53:45 +0700

    refactor: essay store

---
+++ app/stores/essay.ts
@@ -0,0 +1,52 @@
+type State = {
+   ip: string | null
+   answers: {
+      question_id: number,
+      answer: string
+   }[]
+}
+
+export const useEssayStore = defineStore("essaybank-essay-store", {
+   persist: {
+      storage: piniaPluginPersistedstate.localStorage(),
+   },
+
+   state: (): State => ({
+      ip: null,
+      answers: []
+   }),
+
+   getters: {
+      getIp: (state) => (!!state.ip && atob(state.ip)) || null,
+      getAnswers: (state) => state.answers,
+      getAnswer: (state) => (questionId: number) => state.answers.find(a => a.question_id == questionId)
+   },
+
+   actions: {
+      updateIp(ip: IpAddress) {
+         this.ip = btoa(ip)
+      },
+
+      async setIp() {
+         const ipfy = await $fetch<{ ip: IpAddress }>("https://api.ipify.org?format=json")
+         this.updateIp(ipfy.ip)
+      },
+
+      addAnswer(questionId: number, answer: string) {
+         this.answers.push({ question_id: questionId, answer })
+      },
+
+      updateAnswer(questionId: number, answer: string) {
+         const existingAnswer = this.answers.find(a => a.question_id == questionId)
+         if (!existingAnswer) {
+            this.addAnswer(questionId, answer)
+            return
+         }
+         existingAnswer.answer = answer
+      },
+
+      removeAnswer(questionId: number) {
+         this.answers = this.answers.filter(a => a.question_id != questionId)
+      }
+   }
+})
---
+++ layers/utilities/types/ip.d.ts
@@ -0,0 +1,1 @@
+declare type IpAddress = `${number}.${number}.${number}.${number}`
